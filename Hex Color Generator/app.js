94332ae7ba0bec064e2d0cd943bb8543dd0e2363
Umesh Siddarth U S <umeshsiddarth@gmail.com>
2023-03-06 19:42:09 +0530

    Added Random Hex color generator using JS

---
+++ Hex Color Generator/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let hexValue = document.querySelector(".hex");
+const generate = document.querySelector(".generate");
+
+function randomColor() {
+  let randomHex = "#";
+  for (let i = 1; i <= 6; i++) {
+    const randomNum = Math.floor(Math.random() * 16);
+    const hex = randomNum.toString(16);
+    randomHex += hex;
+  }
+  hexValue.textContent = randomHex;
+  document.body.style.backgroundColor = randomHex;
+}
+
+generate.addEventListener("click", randomColor);
