2efb232ae617ebdb24f287120b4bf36243a82ccf
Umesh Siddarth U S <umeshsiddarth@gmail.com>
2023-03-11 19:43:33 +0530

    Added basic DOM selection

--- Password Generator/app.js
+++ Password Generator/app.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const empty = "",
+  uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lCase = "abcdefghijklmnopqrstuvwxyz",
+  number = "0123456789",
+  symbol = "!@#$%^&*=-_";
+
+const passOptLength = document.getElementById("length"),
+  passOptUCase = document.getElementById("uppercase"),
+  passOptLCase = document.getElementById("lowercase"),
+  passOptnumber = document.getElementById("numbers"),
+  passOptsymbol = document.getElementById("symbols"),
+  generateBtn = document.querySelector(".generate"),
+  password = document.querySelector(".password");
+
+generateBtn.addEventListener("click", () => {
+  let initialPassword = empty;
+});
